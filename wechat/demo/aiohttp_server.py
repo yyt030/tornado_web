3f86a3d27b6980c192defd7b10b9939db0f5948f
Yantao <yyt030@gmail.com>
2016-05-20 16:54:48 +0800

    v2

--- wechat/demo/aiohttp_server.py
+++ wechat/demo/aiohttp_server.py
@@ -7,12 +7,20 @@ import asyncio
 from aiohttp import ClientSession
 
 
-async def hello():
+async def hello(url):
     async with ClientSession() as session:
-        async with session.get('https://www.python.org/') as response:
+        async with session.get(url) as response:
             response = await response.read()
             print(response)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(hello())
+tasks = []
+
+url = 'https://www.python.org'
+for i in range(5):
+    task = asyncio.ensure_future(hello(url))
+    tasks.append(task)
+
+
+loop.run_until_complete(asyncio.wait(tasks))
